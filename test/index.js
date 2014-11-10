97a9092cffd27290e6a32b0de8fd3351bd85f779
Shuvalov Anton <anton_shuvalov@me.com>
2014-11-10 03:04:32 +0300

    update id checking in the get method

--- test/index.js
+++ test/index.js
@@ -15,55 +15,86 @@ before(co(function * () {
 
 describe('tasks', function() {
 
-  it('should add tasks', co(function * () {
-    var task1 = yield tasks.add({ name: 'first task', tags: ['inbox'] });
-    var task2 = yield tasks.add({ name: 'second task' });
-  }));
+  describe('add()', function() {
 
-  it('should get all tasks', co(function * () {
-    var list = yield tasks.get();
-    list.length.should.equal(2);
-  }));
+    it('should add tasks', co(function * () {
+      yield tasks.add({ name: 'first task', tags: ['inbox'] });
+    }));
 
-  it('should get tasks by id', co(function * () {
-    var list = yield tasks.get();
-    var task = yield tasks.get(list[0].uid);
-    task.should.have.property('name', 'first task');
-  }));
+    it('should return task document', co(function * () {
+      var task = yield tasks.add({ name: 'second task' });
+      task.should.have.property('uid');
+    }));
 
-  it('should get tasks by tag', co(function * () {
-    var list = yield tasks.get('inbox');
-    list[0].tags.should.have.containEql('inbox');
-  }));
+  });
 
-  it('should update tasks', co(function * () {
-    var list = yield tasks.get();
-    var id = list[0].uid;
-    yield tasks.update(id, { tags: 'save the world' });
-    var updated = yield tasks.get(id);
-    updated.tags.should.containEql('save the world');
-  }));
+  describe('get()', function() {
 
-  it('should delete task', co(function * () {
-    var list = yield tasks.get();
-    var id = list[1].uid;
-    yield tasks.remove(id);
-    list = yield tasks.get();
-    list.length.should.equal(1);
-  }));
+    it('should get all tasks', co(function * () {
+      var list = yield tasks.get();
+      list.length.should.equal(2);
+    }));
 
-  it('should return list of the next tasks', co(function * () {
-    yield tasks.add({ name: '1', due:   +new Date() - 10000 });
-    yield tasks.add({ name: '3', start: +new Date() - 20000 });
-    yield tasks.add({ name: '4', start: +new Date()         });
-    yield tasks.add({ name: '5', start: +new Date() + 10000 });
-    yield tasks.add({ name: '2', start: +new Date() - 50000 });
-    var next = yield tasks.next();
-    var order = next.map(function(i) {
-      return i.name;
-    });
-    order.should.eql(['1', '2', '3', '4']);
-  }));
+    it('should get tasks by id', co(function * () {
+      var list = yield tasks.get();
+      var task = yield tasks.get(list[0].uid);
+      task.should.have.property('name', 'first task');
+    }));
+
+    it('should get tasks by string with id', co(function * () {
+      var list = yield tasks.get();
+      var id = list[0].uid.toString();
+      var task = yield tasks.get(id);
+      task.should.have.property('name', 'first task');
+    }));
+
+    it('should get tasks by tag', co(function * () {
+      var list = yield tasks.get('inbox');
+      list[0].tags.should.have.containEql('inbox');
+    }));
+
+  });
+
+  describe('update()', function() {
+
+    it('should update tasks', co(function * () {
+      var list = yield tasks.get();
+      var id = list[0].uid;
+      yield tasks.update(id, { tags: 'save the world' });
+      var updated = yield tasks.get(id);
+      updated.tags.should.containEql('save the world');
+    }));
+
+  });
+
+  describe('remove()', function() {
+
+    it('should delete task', co(function * () {
+      var list = yield tasks.get();
+      var id = list[1].uid;
+      yield tasks.remove(id);
+      list = yield tasks.get();
+      list.length.should.equal(1);
+    }));
+
+  });
+
+  describe('next()', function() {
+
+    it('should return list of the next tasks', co(function * () {
+      yield tasks.add({ name: '1', due:   +new Date() - 10000 });
+      yield tasks.add({ name: '3', start: +new Date() - 20000 });
+      yield tasks.add({ name: '4', start: +new Date()         });
+      yield tasks.add({ name: '5', start: +new Date() + 10000 });
+      yield tasks.add({ name: '2', start: +new Date() - 50000 });
+      var next = yield tasks.next();
+      var order = next.map(function(i) {
+        return i.name;
+      });
+      order.should.eql(['1', '2', '3', '4']);
+    }));
+
+  });
 
   describe('done()', function() {
 
@@ -95,7 +126,7 @@ describe('tasks', function() {
 
   });
 
-  describe('search()', function() {
+  describe('find()', function() {
 
     before(co(function * () {
       yield tasks.add({ name: 'foo', description: 'bar', tags: ['baz'] });
