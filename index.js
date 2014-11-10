cb1e8424493a053a6f3a587b32222bbd4963d879
Shuvalov Anton <anton_shuvalov@me.com>
2014-11-10 05:53:31 +0300

    use db based on node_env to prevent dropDb by tests

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.set('debug', false);
-mongoose.connect('localhost/tasks');
+mongoose.connect('localhost/tasks_' + process.env.NODE_ENV);
 
 module.exports = process.env.TASKS_COV
   ? require('./lib-cov')
