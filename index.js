4d97955484b32688f6cd6cdb337a23bb7935b2f5
Shuvalov Anton <anton_shuvalov@me.com>
2014-11-10 05:22:53 +0300

    export mongoose model

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-require('mongoose').connect('localhost/tasks');
+var mongoose = require('mongoose');
+
+mongoose.set('debug', false);
+mongoose.connect('localhost/tasks');
+
 module.exports = process.env.TASKS_COV
   ? require('./lib-cov')
   : require('./lib');
